e081e31baaa558aee8a37159f73942db609b4da0
mtc-codes <82724982+mtc-codes@users.noreply.github.com>
2024-10-24 23:19:57 +0000

    First pass; setup complete but need to program formulas. Not totally sure how to proceed.

--- app.rb
+++ app.rb
@@ -2,8 +2,24 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:square)
+end
+
+get("/square/new") do
+  # @base = params.fetch("sq_input").to_fs
+  # @result = @base ** 2
+  
+  erb(:square)
+end
+
+get("/square_root/new") do
+  erb(:squareroot)
+end
+
+get("/random/new") do
+  erb(:random)
+end
+
+get("/payment/new") do
+  erb(:payment)
 end
